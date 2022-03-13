d8ed413e5b78a78e05810392bbb57482c977e832
kufuking14 <kiflomnguse14@gmail.com>
2022-03-13 08:03:38 +0300

    My commit message

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-
+/**
+*main - entry point
+*
+*Return: Always 0 (success/correct)
+*/
 int main(void)
+
 {
-    char ch = 'a';
-    int i = 0;
-    
-	while (i < 16)
-	{
-		if (i < 10)
-		{
-			putchar(i + '0');
-		}
-		else
-		{
-		    putchar(ch);
-		    ch++;
-		}
-		i++;
-	}
-	putchar('\n');
-    return 0;
+char base
+[A[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[_16;
+
+for (base_16 =48; base_16 <=57; base
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C_16++)
+{putchar(base_16);
+}
+
+for (base_16 =97 ; base_16 <= 102; base_16++)
+{
+
+[A[A[C
+putchar(base_16);
+}
+
+putchar ('\n');
+
+}
+[A[A[B[[A
+return (0);
+
 }
