12b9fc601b5b1df002e13c757238ea3aaf5668a5
kufuking14 <kiflomnguse14@gmail.com>
2022-03-13 08:13:33 +0300

    My commit mesage

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,26 +7,21 @@
 int main(void)
 
 {
-char base
-[A[C[C[C[C[C[C[C[C[C
-[A[C[C[C[C[C[C[C[C[C[_16;
+char base_16;
 
-for (base_16 =48; base_16 <=57; base
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C_16++)
+for (base_16 =48; base_16 <=57; base_16++)
 {putchar(base_16);
 }
 
 for (base_16 =97 ; base_16 <= 102; base_16++)
 {
-
-[A[A[C
 putchar(base_16);
 }
 
 putchar ('\n');
 
 }
-[A[A[B[[A
+
 return (0);
 
 }
