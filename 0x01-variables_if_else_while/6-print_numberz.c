b251dcbb8bde47a74a1f178d6d74ccdea8b2e3dd
kufuking14 <kiflomnguse14@gmail.com>
2022-03-13 09:42:56 +0300

    may commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int 1;
+
+[A[A[C[C[C[C[i[C[D[D[D[D[D[D[B[Bfor (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
