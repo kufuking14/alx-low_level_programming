cb13d7723aa163e54647cf63038efadc6d4a3ce8
kufuking14 <kiflomnguse14@gmail.com>
2022-03-16 02:38:24 +0300

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
 #include <unistd.h>
 
 /**
- * 
-
-[A[A[C[C
-
-[A[C[A[A[C[C[C[C[C[C[C[C[C[C[C#include <unistd.h>
-
-/**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
