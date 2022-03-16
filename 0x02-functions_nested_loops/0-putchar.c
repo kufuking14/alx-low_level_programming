ac7315364279c8396e633403a6d79dfd6319ea32
kufuking14 <kiflomnguse14@gmail.com>
2022-03-16 12:36:00 +0300

    My commi f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 **/
 int main(void)
 {
-  char kafss[] = "_putchar";
+char kafss[] = "_putchar";
 
-  int c;
+int c;
 
-  for (c = 0; c < 8; c++)
-  {
+for (c = 0; c < 8; c++)
+ {
     _putchar(kafss[c]);
-  }
-  _putchar('\n');
-  return (0);
+ }
+_putchar('\n');
+return (0);
 }
