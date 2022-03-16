b7ebc5bc023c1ceb370d929fef8f69bc31bd88ed
kufuking14 <kiflomnguse14@gmail.com>
2022-03-16 12:30:46 +0300

    My commi f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
  * main - entry block
  * @void: no argument
  * Return: 0
- /
+**/
 int main(void)
 {
-  char ermias[] = "_putchar";
+  char kafss[] = "_putchar";
 
   int c;
 
   for (c = 0; c < 8; c++)
   {
-    _putchar(ermias[c]);
+    _putchar(kafss[c]);
   }
   _putchar('\n');
   return (0);
