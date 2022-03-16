4c6a6b0b026d22c0f1ea76e3368dd1a000c08dc1
kufuking14 <kiflomnguse14@gmail.com>
2022-03-16 12:50:31 +0300

    My commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet - prints alphabet in lowercase
+*
+**/
+void print
+[A[C[C[C[C[C[C[C[C[C[C_alphabet(void)
+{
+char c;
+
+for (c = 'a' c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
