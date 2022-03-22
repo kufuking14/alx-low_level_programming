5cdeb1e9e9ccf6af4083a09271a477f23ddaab31
kufuking14 <kiflomnguse14@gmail.com>
2022-03-22 04:48:57 +0300

    My zero commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - reset argument to 98
- * @n: - Pointer to int
- * Return: void
- */
+* reset_to_98 - reset argument to 98
+* @n: - Pointer to int
+* Return: void
+*/
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
