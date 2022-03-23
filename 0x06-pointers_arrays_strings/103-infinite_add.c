dba2a338baaa09e543ca16dd859365cd678cc4c4
kufuking14 <kiflomnguse14@gmail.com>
2022-03-23 21:46:17 +0300

    My commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,7 +3,7 @@
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-/
+/**
  * add_strings - Adds the numbers stored in two strings.
  * @n1: The string containing the first number to be added.
  * @n2: The string containing the second number to be added.
@@ -50,7 +50,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
   return (r + r_index + 1);
 }
-/
+/**
  * infinite_add - Adds two numbers.
  * @n1: The first number to be added.
  * @n2: The second number to be added.
