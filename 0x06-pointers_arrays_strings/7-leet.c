9f9f92740eff52ce06d0f814231ab7b6b6e8bf0f
kufuking14 <kiflomnguse14@gmail.com>
2022-03-23 21:25:31 +0300

    My commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,33 +1,27 @@
 #include "main.h"
+
 /**
-* leet -  encodes a string into 1337.
-* @s: string to code
-* Return: the enconde string.
-*/
-char *leet(char *s)
+ * rot13 -  a function that encodes a string using rot13.
+ * @s: An input string to encode using rot13
+ * Return: An encode string
+ */
+char *rot13(char *s)
 {
-int i;
-int j;
-int k;
+  int i = 0;
 
-char l[] = "oOlLeEaAtT";
-char e[] = "0011334477";
-
-i = 0;
-while (s[i] != '\0')
-{
-j = 0;
-k = 0;
-while (l[j] != '\0')
-{
-if (s[i] == l[j])
-{
-k = j;
-s[i] = e[k];
-}
-j++;
-}
-i++;
-}
-return (s);
+  while (s[i] != '\0')
+  {
+    while ((s[i] >= 'a' && s[i] <= 'z') 
+        (s[i] >= 'A' && s[i] <= 'Z'))
+    {
+      if ((s[i] >= 'a' && s[i] <= 'm') 
+          (s[i] >= 'A' && s[i] <= 'M'))
+        s[i] += 13;
+      else
+        s[i] -= 13;
+      i++;
+    }
+    i++;
+  }
+  return (s);
 }
