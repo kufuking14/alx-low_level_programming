f7096fa4187a0595bed1197db6400277f1c38833
kufuking14 <kiflomnguse14@gmail.com>
2022-04-01 20:25:58 +0300

    My commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h.h"
 #include <stdlib.h>
-
 /**
  * main - program that prints its name
  *
@@ -11,26 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
-  int s, x = 0;
-  char *a;
-  int b;
+	int s, x = 0;
+	char *a;
+	int b;
 
-  if (argc <= 2)
-  {
-    printf("0\n");
-    return (0);
-  }
-  for (s = 1; s < argc; s++)
-  {
-    a = argv[s];
-    for (b = 0; a[b] != '\0'; b++)
-      if (a[b] > '9' || a[b] < '0')
-      {
-        printf("Error\n");
-        return (1);
-      }
-    x += atoi(argv[s]);
-  }
-  printf("%d\n", x);
-  return (0);
+	if (argc <= 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (s = 1; s < argc; s++)
+	{
+		a = argv[s];
+		for (b = 0; a[b] != '\0'; b++)
+			if (a[b] > '9' || a[b] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		x += atoi(argv[s]);
+	}
+	printf("%d\n", x);
+	return (0);
 }
